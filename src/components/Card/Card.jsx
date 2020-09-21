23d5e35a2ba8175a9e2da751a69e2d0b87b4419c
GiuseppeScalese <scalese.giuseppe@gmail.com>
2020-09-21 15:51:16 +0100

    added card link title

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,10 +4,10 @@ import Image from '../Image/Image';
 import styles from './Card.module.scss';
 
 const Card = ({
-  cardLink, cardName, cardImage, cardCopy
+  cardLink, cardTitle, cardName, cardImage, cardCopy
 }) => {
   return (
-    <a className={`${styles.card}`} href={cardLink} title={cardName}>
+    <a className={`${styles.card}`} href={cardLink} title={cardTitle}>
       <Image source={cardImage} size="fit" altText={cardName} />
       <span className={`${styles['card-copy']}`}>{cardCopy}</span>
     </a>
